a2f18f04a09efb4bc6d7a23ec9f41f21b573bbdf
Mickael Temporão <16172142+mickaeltemporao@users.noreply.github.com>
2024-04-15 15:19:27 +0200

    update readme description

--- src/subsample.py
+++ src/subsample.py
@@ -13,9 +13,10 @@ load_dotenv(find_dotenv())
 
 
 DATA_PATH = os.environ.get("DATA_PATH")
-USER_FILE = os.environ.get("USER_FILE")
+DATA_FILE = DATA_PATH + "tmp/gpt_classifier_output.csv"
 OUTPUT_FILE_PATH = DATA_PATH + "tmp/twitter_account_sample_10pct.csv"
 
-df = pd.read_csv(DATA_PATH+USER_FILE)
-df
+cols = ['username', 'description']
+df = pd.read_csv(DATA_FILE).sample(frac=.10)[cols]
+df.to_csv(OUTPUT_FILE_PATH, index=False)
 
