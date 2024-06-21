286d104764a03c63979b811d9e123f13f28bf5bd
Mickael Temporão <16172142+mickaeltemporao@users.noreply.github.com>
2024-06-21 11:10:58 +0200

    function ready

--- src/make_tweet_csv.py
+++ src/make_tweet_csv.py
@@ -3,22 +3,35 @@ import os
 import numpy as np
 import pandas as pd
 
+from tqdm import tqdm
 
-path_to_json = 'data/tweets-Presidentielle2022/'
-json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
 
-with open(path_to_json+json_files[0]) as f:
-    d = json.load(f)
+def main():
+    path_to_json = 'data/tweets-Presidentielle2022/'
+    json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
+    cols = ['id', 'created_at', 'author_id', 'referenced_tweets']
+    new_cols = ['id', 'created_at', 'author_id', 'referenced_tweets', 'type', 'ref_id']
 
-df = pd.json_normalize(d['data'], max_level=2)
-cols = ['id', 'created_at', 'author_id', 'referenced_tweets']
-df = df[cols]
+    file_1 = json_files[0]
+    file_2 = json_files[-1]
 
-mask = df['referenced_tweets'].isna()
-df['referenced_tweets'] = df['referenced_tweets'].ffill()
-df['referenced_tweets'] = df['referenced_tweets'].apply(lambda x: x[0])
-df = pd.concat([df, df['referenced_tweets'].apply(pd.Series)], axis=1)
-df.columns = ['id', 'created_at', 'author_id', 'referenced_tweets', 'type', 'ref_id']
-df.loc[mask, 'type'] = 'tweet'
-df.loc[mask, 'ref_id'] = np.nan
+    all_dfs = []
+    all_dfs
+    for file in tqdm(json_files):
+        with open(path_to_json+file_2) as f:
+            d = json.load(f)
+            df = pd.json_normalize(d['data'], max_level=2)
+            df = df[cols]
 
+        mask = df['referenced_tweets'].isna()
+        df['referenced_tweets'] = df['referenced_tweets'].ffill()
+        df['referenced_tweets'] = df['referenced_tweets'].apply(lambda x: x[0])
+        df = pd.concat([df, df['referenced_tweets'].apply(pd.Series)], axis=1)
+        df.columns = new_cols
+        df.loc[mask, 'type'] = 'tweet'
+        df.loc[mask, 'ref_id'] = np.nan
+        all_dfs.append(df)
+    pd.concat(all_dfs).to_csv('data/tmp/tweets.csv')
+
+if __name__ == "__main__":
+    main()
