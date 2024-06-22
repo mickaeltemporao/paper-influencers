541726e75f3f44ab6867c4b4f7ec91b7744fe9be
Mickael Temporão <16172142+mickaeltemporao@users.noreply.github.com>
2024-06-22 21:40:37 +0200

    adding lookforward for conversion rate

--- src/kfactor.py
+++ src/kfactor.py
@@ -17,8 +17,24 @@ dtypes = {
 users = pd.read_csv("data/tmp/twitter-users.csv")
 df = pd.read_csv("data/tmp/tweets.csv", dtype=dtypes)
 df['created_at'] = pd.to_datetime(df['created_at'])
+
+election_date = pd.to_datetime("2022-04-11").tz_localize('UTC')
+df['created_at'] = pd.to_datetime(df['created_at'], utc=True)
+df = df[df['created_at'] <= election_date]
 df['date'] = df['created_at'].dt.date
-df
+
+# Find Top 1% active accounts in the last 2 weeks 
+group = ['date', 'author_id']
+two_weeks = pd.to_datetime("2022-04-10") - pd.Timedelta('2w')
+top_df = df.groupby(group).size().reset_index(name='msg_count')
+mask = pd.to_datetime(top_df['date']) > two_weeks
+top_df = top_df[mask]
+top_df = top_df.set_index(group)
+tmp_df = top_df.reset_index().groupby('author_id').size()
+mask = tmp_df > tmp_df.quantile(0.99)
+top_authors = tmp_df[mask].index.values
+del tmp_df
+
 
 # TODO:
 # Add momentum parameter: Sample top m days % change kfacotrs
@@ -29,29 +45,32 @@ df
 # optimize N vs inductive
 
 # Switch to CONVERSION RATE
-def get_k_factor(df, author, date):
+def get_k_factor(df, author, date, hours=24):
     # Find tweets from author
     mask1 = df['author_id'] == author
-    # Filter before date
+    # Get tweets from author up to date
     mask2 = df['date'] <= date
-    # Get the list of ids for user until date
+    # Get the list of ids for user on date
     user_tw_list = df.loc[(mask1 & mask2), 'id'].unique()
+    # Conversion Date Range
+    mask3 = df['date'] <= date + pd.Timedelta(f'{hours}h')
     # i = number of tweets sent by user
     i = len(user_tw_list)
+    if i <= 0:
+        return 0
     # n_int = number of interactions from other users
-    n_interactions = df[(~mask1 & mask2)]['ref_id'].isin(user_tw_list).sum()
+    n_interactions = df[(~mask1 & mask3)]['ref_id'].isin(user_tw_list).sum()
     # c = percent conversion of each tweet
     if n_interactions > 0:
-        c = n_interactions/i
+        return n_interactions/i
     else:
-        c = 0
-    # k-factor
-    return c
+        return 0
 
 
-list_of_dates = df['date'].unique().sort()
+list_of_dates = df['date'].unique()
+list_of_dates.sort()
 k_factors_list = []
-day = pd.to_datetime('2022-03-10').date()
+day = pd.to_datetime('2022-02-01').date()
 user = 772215918868979712
 get_k_factor(df, user, day)
 
@@ -59,8 +78,9 @@ get_k_factor(df, user, day)
 def task(day):
     print(f"Starting task {day}.")
     tic = process_time()
-    mask = df['date'] <= day
-    list_of_users = df.loc[df['date'] == day, 'author_id'].unique()
+    mask1 = df['date'] <= day
+    mask2 = df['author_id'].isin(top_authors)
+    list_of_users = df.loc[(mask1 & mask2), 'author_id'].unique()
     tmp_output = []
     for user in list_of_users:
         tmp_output.append({'author_id': user, 'date': day, 'k_factor': get_k_factor(df, user, day)})
@@ -71,10 +91,8 @@ def task(day):
 # create a process pool that uses all cpus -1
 cpus = multiprocessing.cpu_count()-1
 with multiprocessing.Pool(cpus) as pool:
-    for result in pool.map(task, list_of_dates[0:2]):
-        k_factors_list.append(result)
+    for result in pool.map(task, list_of_dates):
+        k_factors_list.append(pd.DataFrame(result))
 
 # Save the k-factors DataFrame to a CSV file
-k_factors_df = pd.DataFrame(k_factors_list)
-k_factors_df.to_csv('daily_k_factors.csv', index=False)
-
+pd.concat(k_factors_list).to_csv('data/tmp/daily_k_factors.csv', index=False)
