a6e2872545ab8fa2009bebe4bbc4e3d8e7f5f941
Mickael Temporão <16172142+mickaeltemporao@users.noreply.github.com>
2024-08-02 20:33:29 +0200

    initial in degree centrality

---
+++ src/network-analysis.py
@@ -0,0 +1,23 @@
+import networkx as nx  # TODO: REMOVE NETWORKX
+import pandas as pd
+import igraph as ig
+from matplotlib import pyplot as plt
+
+
+df = pd.read_csv("data/tmp/network.csv")
+g = ig.Graph.TupleList(df[['author_id', 'mentions']].itertuples(index=False), directed=True)
+
+# We look at in degree centrality as we want to get the most retweeted users
+estimate = g.degree(mode="in")
+
+output = pd.DataFrame(
+    {
+        "author_id": g.vs["name"],
+        "centrality": estimate,
+     }
+)
+
+output['centrality'] = output['centrality']/(output.shape[0]-1)
+mask = output['centrality'] > output['centrality'].quantile(0.990)
+output[mask]
+
